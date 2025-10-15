b34c94ab2747ce2069ea8d356b84abd4f2106a0f
Amy <emysgalippa@gmail.com>
2025-10-15 21:29:07 +0200

    init

---
+++ server.ts
@@ -0,0 +1,29 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express, { Request, Response } from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({ message: "Hello world" });
+});
+
+app.get("/health", (req: Request, res: Response) => {
+  res.json({
+    status: "Server is healthy",
+    timestamp: new Date().toISOString(),
+  });
+});
+
+app.use((req: Request, res: Response) => {
+  res.status(404).json({ error: "Route not found" });
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server in esecuzione su http://localhost:${PORT}`);
+});
